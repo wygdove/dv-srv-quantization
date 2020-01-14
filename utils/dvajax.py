995e1cfe95f7ea1ef1bd4c93f509dfe02a50a38a
weiyg <weiyg@asiainfo.com>
2020-01-14 11:18:40 +0800

    rename message

--- utils/dvajax.py
+++ utils/dvajax.py
@@ -4,14 +4,14 @@ __time__='2020/1/6 16:11'
 
 
 resultCode={"success":"000000","error":"999999","user_null":"100001","request_null":"200001","data_null":"800001"}
-result={"code":"000000","result":None,"msg":"success","info":""}
+result={"code":"000000","result":None,"message":"success","info":""}
 
 def success(obj):
     if obj==None:
         return nodata()
     result["code"]=resultCode["success"]
     result["result"]=obj
-    result["msg"]="success"
+    result["message"]="success"
     result["info"]=""
     return result
 
@@ -19,7 +19,7 @@ def success(obj):
 def error(msg,info):
     result["code"]=resultCode["error"]
     result["result"]=None
-    result["msg"]=msg
+    result["message"]=msg
     result["info"]=info
     return result
 
@@ -31,21 +31,21 @@ def error(msg,info):
 def nouser():
     result["code"]=resultCode["user_null"]
     result["result"]=None
-    result["msg"]=""
+    result["message"]=""
     result["info"]="user is null"
     return result
 
 def norequest():
     result["code"]=resultCode["request_null"]
     result["result"]=None
-    result["msg"]=""
+    result["message"]=""
     result["info"]="request is null"
     return result
 
 def nodata():
     result["code"]=resultCode["data_null"]
     result["result"]=None
-    result["msg"]=""
+    result["message"]=""
     result["info"]="result is null"
     return result
 
