4e51c8b69760e4e24712f91199a2c6c75b85b6e5
weiyg <weiyg@asiainfo.com>
2020-03-22 19:46:44 +0800

    test

--- test/test_item.py
+++ test/test_item.py
@@ -5,30 +5,71 @@ __time__='2020/1/22 19:00'
 
 
 from utils import dvtest
-from modules import item
+from modules import item as Item
+
+
+datas=''''''
 
 
 
 if __name__=='__main__':
     itemConfig={
-        "moduleFlag":"AssetRecord",
-        "codeKey":"recordCode",
-        "codePrefix":"AR",
+        "moduleFlag":"Transaction",
+        "codeKey":"transactionCode",
+        "codePrefix":"TR",
         "codeLength":12,
     }
-
     itemData={
-        # "recordCode":"",
-        "recordDate":"2019-08-22 13:32:12",
-        "recordInOut":"100",
-        "recordHolding":"0",
+        "transactionCode":"TR000000000012",
+        "transactionDate":"2020/02/26 23:30:03",
+        "investmentCode":"110063",
+        "investmentName":"鹰19转债",
+        "investClassCode":"ConvertibleBond",
+        "investClassName":"可转债",
+        "investmentBuysell":0,
+        "investmentEarnHistory":0,
+        "investmentHolding":100,
+        "remark":"",
         "accountCode":"UA000031",
     }
-    # item.saveItem(itemConfig,itemData)
-
-    item.deleteItem(itemConfig,{itemConfig["codeKey"]:"AR000000000003"})
 
 
-    res=item.getItems(itemConfig)
-    dvtest.tprint(res)
+
+
+
+
+
+    # Item.saveItem(itemConfig,itemData)
+
+    # Item.deleteItem(itemConfig,{itemConfig["codeKey"]:"AR000000000119"})
+    for i in xrange(3,32):
+        break
+        # code='AR000000000'+str(i)
+        # Item.deleteItem(itemConfig,{itemConfig["codeKey"]:code})
+
+    dataArr=datas.split('\n')
+    for data in dataArr:
+        break
+        da=data.split('\t')
+        transactionDate=da[0]
+        investmentName=da[1]
+        investmentBuysell=float(da[2])
+        itemData={
+            "transactionDate":transactionDate,
+            "investmentCode":'',
+            "investmentName":investmentName,
+            "investClassCode":'',
+            "investClassName":'',
+            "investmentBuysell":investmentBuysell,
+            "investmentHolding":0,
+            "investmentEarnHistory":0,
+            "remark":"",
+            "accountCode":"UA000031"
+        }
+        # print itemData
+        # Item.saveItem(itemConfig,itemData)
+
+    # dvtest.tprint(Item.getItems(itemConfig,{}))
+
+    dvtest.tprint(Item.getItems({"moduleFlag":"Investment","codeKey":"investmentIdCode"},{}))
 
--- utils/dvtest.py
+++ utils/dvtest.py
@@ -10,3 +10,8 @@ import json
 def tprint(object):
     print json.dumps(object,encoding='utf-8',ensure_ascii=False)
 
+def presult(object):
+    data=json.dumps(object,encoding='utf-8',ensure_ascii=False)
+    result=json.loads(data)['result']
+    return json.dumps(result,encoding='utf-8',ensure_ascii=False)
+
